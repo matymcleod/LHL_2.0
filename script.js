e1ec39da54b7cf58c7e4928d0bf95b2df06b962e
matymcleod <matymcleod@gmail.com>
2024-12-18 16:58:15 -0800

    adding arraySum to test my js memory

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const testArray = [1, 2, 3]
+const testArray = [1, 2, 3];
+
+console.log('testArray', testArray);
+
+
+let arraySum;
+console.log('arraySum before adding test array', arraySum);
+for(let i=0; i<testArray.length; i++){
+  arraySum += testArray[i]
+}
+console.log('arraySum after adding test array', arraySum);
