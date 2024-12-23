ea7b35108ff1885b57b7cd68c57393f3cceb2b23
matymcleod <matymcleod@gmail.com>
2024-12-22 19:20:54 -0800

    playing with js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const testArray = [1, 2, 3];
-
+/*const testArray = [1, 2, 3];
 console.log('testArray', testArray);
-
-
 let arraySum;
 console.log('arraySum before adding test array', arraySum);
 for(let i=0; i<testArray.length; i++){
   arraySum += testArray[i]
 }
-console.log('arraySum after adding test array', arraySum);
+console.log('arraySum after adding test array', arraySum);*/
+
+// let num = 1;
+
+// while(num <= 20){
+//   if(num % 2 === 0) {
+//     console.log(num);
+//   }
+//   num++
+// }
+
