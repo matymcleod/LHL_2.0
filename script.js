0ffa2c08374f4f2f193698320c8195e1083ae052
matymcleod <matymcleod@gmail.com>
2024-12-23 00:34:16 -0800

    while and for loops

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Web prep course
+
 /*const testArray = [1, 2, 3];
 console.log('testArray', testArray);
 let arraySum;
@@ -7,12 +9,45 @@ for(let i=0; i<testArray.length; i++){
 }
 console.log('arraySum after adding test array', arraySum);*/
 
-// let num = 1;
+//JS Fundamentals ||
+//Construction Loops
+// Q1 - Write a program that prints the sum of all odd numbers from 1 to 50 using a for loop.
 
-// while(num <= 20){
-//   if(num % 2 === 0) {
-//     console.log(num);
+// let sum = 0;
+// for(let odd = 0; odd < 50; odd++){
+//   if(odd % 2 !== 0) {
+//     sum += odd
 //   }
+// }
+// console.log(sum);
+
+// Q2 - Write a program that prints the even numbers from 1 to 20 using a while loop
+// let num = 1;
+// while(num <= 20){
+//   if(num % 2 === 0){
+//   console.log(num);}
 //   num++
+// }
+
+// Q3 - Write a program that prints the numbers from 5 - 15 using a for loop  
+
+// for(let num = 5; num <= 15; num++){
+//   console.log(num);
+// }
+
+// Q4 - Write a program that prints the sum of all numbers from 1 to 50.
+
+// let sum = 0;
+// let num = 1;
+// while(num <= 50){
+//   sum += num
+//   num++
+// }
+// console.log(sum);
+
+// Q5 - Write a program that prints a countdown from 10 to 1 using a for loop.
+// let count = 0;
+// for(let count = 10; count >= 1; count--){
+//   console.log(count);
 // }
 
