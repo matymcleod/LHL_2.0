578461b9b172d570bdd33fe0c6aa4ea13c338300
matymcleod <matymcleod@gmail.com>
2024-12-27 18:48:34 -0800

    Beacon countdown

---
+++ prep_work/script.js
@@ -0,0 +1,66 @@
+//Web prep course
+
+/*const testArray = [1, 2, 3];
+console.log('testArray', testArray);
+let arraySum;
+console.log('arraySum before adding test array', arraySum);
+for(let i=0; i<testArray.length; i++){
+  arraySum += testArray[i]
+}
+console.log('arraySum after adding test array', arraySum);*/
+
+//JS Fundamentals ||
+//Construction Loops
+// Q1 - Write a program that prints the sum of all odd numbers from 1 to 50 using a for loop.
+
+// let sum = 0;
+// for(let odd = 0; odd < 50; odd++){
+//   if(odd % 2 !== 0) {
+//     sum += odd
+//   }
+// }
+// console.log(sum);
+
+// Q2 - Write a program that prints the even numbers from 1 to 20 using a while loop
+// let num = 1;
+// while(num <= 20){
+//   if(num % 2 === 0){
+//   console.log(num);}
+//   num++
+// }
+
+// Q3 - Write a program that prints the numbers from 5 - 15 using a for loop  
+
+// for(let num = 5; num <= 15; num++){
+//   console.log(num);
+// }
+
+// Q4 - Write a program that prints the sum of all numbers from 1 to 50.
+
+// let sum = 0;
+// let num = 1;
+// while(num <= 50){
+//   sum += num
+//   num++
+// }
+// console.log(sum);
+
+// Q5 - Write a program that prints a countdown from 10 to 1 using a for loop.
+// for(let count = 10; count >= 1; count--){
+//   console.log(count);
+// }
+
+// BEACON COUNTDOWN - 
+// let num = 60;
+// while(num <= 60 ){
+//   if(num >= 0){
+//     console.log(num)
+//   }
+//   if(num === 60) {console.log(": One minute to lift off");}
+//   else if(num > 10) {console.log(": All systems green");}
+//   else if(num >=6 && num <=10) {console.log(": Starting ignition");}
+//   else if(num >=3 && num <=5) {console.log(": Ignition Complete");}
+//   else if(num >=1 && num <=2) {console.log(": Full Power");}
+//   else if(num === 0) {console.log(": Lift Off");}
+//   num--
+// }
